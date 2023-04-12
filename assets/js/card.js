85d52db6eb62238e131b6031d264e9844fc999b4
FelixTheSapiens <114573071+FelixTheSapiens@users.noreply.github.com>
2023-04-12 12:43:41 +0400

    fixed shool showing as [Object Object]

--- assets/js/card.js
+++ assets/js/card.js
@@ -9,7 +9,7 @@ class Card {
             <div class="card-body">
                 <h3 class="spellText bold">${this.spellData.name}<br></h3>
                 <p class="spellText text-nowrap d-inline">lvl ${this.spellData.card.level}</p>
-                <p class="spellText text-nowrap d-inline">${this.spellData.card.school}</p>
+                <p class="spellText text-nowrap d-inline">${this.spellData.card.school.name}</p>
                 <p class="spellText text-nowrap d-inline">${this.spellData.card.casting_time}</p>
             </div>
         </div>`;
